1bdc251d8dc0cf14785d7563deb989f4a67a5061
Roger Camargo <huogerac@gmail.com>
2017-09-05 17:31:05 -0300

    Added ALLOWED_HOSTS to avoid server header error

--- mysitezero/settings.py
+++ mysitezero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ic$%8u0a(mmosez3vtl(h2aygx772q77uu_f8*wc+hk09qabf7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
